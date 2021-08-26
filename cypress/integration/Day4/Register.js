cc4ff20dce56b2c5e314fc401949a82ed4ab04d9
Farhan M R A <48043106+farhanmra@users.noreply.github.com>
2021-08-26 21:26:04 +0700

    Update Register.js

--- cypress/integration/Day4/Register.js
+++ cypress/integration/Day4/Register.js
@@ -1,6 +1,9 @@
 describe('kumpulan test case', function () {
-    it('register    ', function(){
+    before(()=>{
         cy.bukaUrl()
+    })
+    it('register    ', function(){
+        
         cy.get('#signin2').click()
         cy.wait(1000)
         cy.get('#sign-username').type('farhantest123').should('have.value','farhantest123')
@@ -8,7 +11,7 @@ describe('kumpulan test case', function () {
         cy.wait(1000)
         cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
         cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-secondary').click()
-       
+
     })
 
     Cypress.on('uncaught:exception', (err, runnable)=>{
